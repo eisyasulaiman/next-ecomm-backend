096abdc7402de27d2144631aab1587eadb9a4592
Aisyah Sulaiman <aisyahsulaiman98@gmail.com>
2023-11-24 17:11:49 +0800

    added try/catch error in index.js

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
 import express from "express"
 import prisma from "./src/utils/prisma.js"
+import { Prisma, PrismaClient } from '@prisma/client'
 
 const app = express()
 const port = process.env.PORT || 8080
 
 app.use(express.json())
 app.get('/', async ( res) => {
-  const allUsers = await prisma.user.findMany()
-  res.json(allUsers)
+  const allUsers = await prisma.User.findMany()
+  res.json(allUsers) 
 })
 
-app.get('/user/:id', async (req, res) => {
-  const { id } = req.params
-  const user = await prisma.user.findUnique({
-    where: { id: Number(id) },
-  })
-  res.json(user)
-})
-
-app.post('/', async (req, res) => {
-    const { name, email, password } = req.body
-    const newUser = await prisma.user.create({
+app.post('/users', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const newUser = await prisma.User.create({
       data: { name, email, password },
-    })
+    });
   
-    console.log(newUser)
+    console.log(newUser);
 
-    res.json(newUser)
-  })
+    res.json(newUser);
+  } catch (err) {
+    if (err instanceof Prisma.PrismaClientKnownRequestError && err.code === 'P2002') {
+      const formattedError = {};
+      formattedError[`${err.meta.target[0]}`] = 'already taken';
 
-  app.put('/user/:id', async (req, res) => {
-    const { id } = req.params
-    const user = await prisma.user.update({
-      where: { id: Number(id) },
-      data: req.body,
-    })
-    
-    res.json(user)
-  })
+      return res.status(500).json({
+        error: formattedError
+      });  // friendly error handling
+    }
 
+    // to log it for debugging purposes
+    console.error(err);
+
+    // Send a generic error response
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 app.listen(port, function (err) {
   if (err) console.log(err);
-  console.log("Server listening on PORT ${port}");
+  console.log(`Server listening on PORT ${port}`);
 })
